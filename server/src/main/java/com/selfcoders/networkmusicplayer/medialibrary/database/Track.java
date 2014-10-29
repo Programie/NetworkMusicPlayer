a2e05e35356368c65e21b2f56b8de4bec64a8193
Michael Wieland <github@selfcoders.com>
2014-10-29 22:38:56 +0100

    Fixed SQL exceptions

--- server/src/main/java/com/selfcoders/networkmusicplayer/medialibrary/database/Track.java
+++ server/src/main/java/com/selfcoders/networkmusicplayer/medialibrary/database/Track.java
@@ -3,6 +3,8 @@ package com.selfcoders.networkmusicplayer.medialibrary.database;
 import com.selfcoders.networkmusicplayer.medialibrary.MediaMetadata;
 import java.io.File;
 import java.io.IOException;
+
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -17,15 +19,22 @@ public class Track {
         MapSqlParameterSource parameters = new MapSqlParameterSource();
         parameters.addValue("filePath", filePath);
 
-        return jdbcTemplate.queryForObject("SELECT `id` FROM `tracks` WHERE `filePathHash` = UNHEX(MD5(:filePath))",
-                parameters, Integer.class);
+        try {
+            return jdbcTemplate.queryForObject("SELECT `id` FROM `tracks` WHERE `filePathHash` = UNHEX(MD5(:filePath))", parameters, Integer.class);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     public String getFilePathByTrackId(int trackId) {
         MapSqlParameterSource parameters = new MapSqlParameterSource();
         parameters.addValue("id", trackId);
 
-        return jdbcTemplate.queryForObject("SELECT `filePath` FROM `tracks` WHERE `id` = :id", parameters, String.class);
+        try {
+            return jdbcTemplate.queryForObject("SELECT `filePath` FROM `tracks` WHERE `id` = :id", parameters, String.class);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     public boolean updateTrackData(int trackId) throws IOException {
@@ -55,18 +64,18 @@ public class Track {
         parameters.addValue("id", trackId);
 
         jdbcTemplate.update(
-                "UPDATE `tracks`" +
-                "SET" +
-                "`title` = :title," +
-                "`artist` = :artist," +
-                "`album` = :album," +
-                "`genre` = :genre," +
-                "`year` = :year," +
-                "`trackNumber` = :trackNumber," +
-                "`trackCount` = :trackCount," +
-                "`diskNumber` = :diskNumber," +
-                "`diskCount` = :diskCount," +
-                "`length` = :length" +
+                "UPDATE `tracks` " +
+                "SET " +
+                "`title` = :title, " +
+                "`artist` = :artist, " +
+                "`album` = :album, " +
+                "`genre` = :genre, " +
+                "`year` = :year, " +
+                "`trackNumber` = :trackNumber, " +
+                "`trackCount` = :trackCount, " +
+                "`diskNumber` = :diskNumber, " +
+                "`diskCount` = :diskCount, " +
+                "`length` = :length " +
                 "WHERE `id` = :id", parameters);
 
         return true;
@@ -76,6 +85,6 @@ public class Track {
         MapSqlParameterSource parameters = new MapSqlParameterSource();
         parameters.addValue("filePath", filePath);
 
-        jdbcTemplate.update("INSERT INTO `tracks` SET `filePath` = :filePath, `filePathHash` = UNHEX(MD5(:filePath))", parameters);
+        jdbcTemplate.update("INSERT INTO `tracks` SET `filePath` = :filePath, `filePathHash` = UNHEX(MD5(:filePath)), `dateAdded` = NOW()", parameters);
     }
 }
