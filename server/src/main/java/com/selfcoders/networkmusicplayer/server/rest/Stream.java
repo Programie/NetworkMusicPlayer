545c788618cb2055dbb57ef216f6c28eaefdfab5
Michael Wieland <github@selfcoders.com>
2014-10-30 21:53:00 +0100

    Fixed error (value should be replaced with value from database)

--- server/src/main/java/com/selfcoders/networkmusicplayer/server/rest/Stream.java
+++ server/src/main/java/com/selfcoders/networkmusicplayer/server/rest/Stream.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.io.File;
 import java.io.IOException;
 
 @Path("/stream/{id:\\d+}")
@@ -15,7 +16,7 @@ public class Stream {
     @GET
     @Produces("audio/mp3")
     public Response get(@HeaderParam("Range") String range, @PathParam("id") int id) throws IOException {
-        // TODO: Get file by ID (Requires database connection)
+        File file = null;// TODO: Get file by ID (Requires database connection)
         HttpStreamBuilder streamBuilder = new HttpStreamBuilder(file, 1024 * 1024);
 
         return streamBuilder.build(range);
